d76aa2f5b2628910414795abb8019a0543b2738a
abemaru <ch4nge.lightening@gmail.com>
2022-10-19 08:22:53 +0900

    毎回違う文字列が出るように変更

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"flag"
+	"crypto/rand"
 )
 
 func main() {
-	generator()
+	p := generator()
+	fmt.Printf(p)
 }
 
-func generator() {
-	var (
-		l = flag.Int("l", 6, "password length")
-	)
+const rs2Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func generator() string {
+	var pwLen = flag.Int("l", 6, "password length")
+	
 	flag.Parse()
-	fmt.Printf("param -l : %s\n", *l)
+
+	b := make([]byte, *pwLen)
+	for i := range b {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(rs2Letters))))
+		if err != nil {
+			panic(err)
+		}
+		b[i] = rs2Letters[n.Int64()]
+	}
+	return string(b)
 }
